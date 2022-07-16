2b79b6b2d24c9d9194b8eb489ba6a9899765fc20
Santiago Gaviria <santi-skate@hotmail.com>
2022-07-16 03:01:45 -0500

    Generating tweet already!

--- src/app/test.js
+++ src/app/test.js
@@ -2,6 +2,8 @@ import got from 'got';
 import crypto from 'crypto';
 import OAuth from 'oauth-1.0a';
 import dotenv from 'dotenv';
+import genius from 'genius-lyrics-api';
+import { songs } from './songs.js'
 dotenv.config();
 
 
@@ -12,6 +14,13 @@ const oAuthAccessToken = {
   oauth_token: process.env.TWITTER_OAUTH_TOKEN,
   oauth_token_secret: process.env.TWITTER_OAUTH_TOKEN_SECRET
 }
+
+const options = {
+	apiKey: process.env.GENIUS_ACCESS_TOKEN,
+  title: undefined,
+	artist: 'Julio Jaramillo',
+	optimizeQuery: true
+};
 
 const data = {
   "text": "Hello world!"
@@ -29,10 +38,49 @@ const oauth = OAuth({
   hash_function: (baseString, key) => crypto.createHmac('sha1', key).update(baseString).digest('base64')
 });
 
-async function getRequest({
-  oauth_token,
-  oauth_token_secret
-}) {
+async function getRawLyrics() {
+  const randomSongTitle = getRandomTitle();
+  options.title = randomSongTitle;
+  return await genius.getLyrics(options)
+}
+
+function getRandomTitle() {
+  return songs[Math.floor(Math.random() * songs.length)]
+}
+
+function filterLyrics(lyrics) {
+  lyrics = lyrics.split('\n');
+  lyrics = lyrics.filter(Boolean)
+  lyrics = lyrics.filter(line => !(line.startsWith('(') || line.startsWith('*')));
+  lyrics = lyrics.filter(line => !(line.startsWith('[') || line.toLowerCase().trim() == 'coro'));
+  return lyrics;
+}
+
+function pickRandomNumberForLines() {
+  return Math.floor(Math.random() * 3) + 2;
+}
+
+function getRandomIndex(size, numberOfLines) {
+  return Math.floor(Math.random() * (size - numberOfLines))
+}
+
+function getTweetFrom(lyrics) {
+  const numberOfLines = pickRandomNumberForLines();
+  const index = getRandomIndex(lyrics.length, numberOfLines);
+  let tweet = '';
+  for (let i = index; i < index + numberOfLines; i++) {
+    if(i === index + numberOfLines - 1) tweet = tweet + lyrics[i]
+    else tweet = tweet + lyrics[i] + '/n';
+  }
+  tweet = tweet.replace('\\', '');
+  return tweet;
+}
+
+
+
+async function getRequest(
+  { oauth_token,
+    oauth_token_secret }) {
 
   const token = {
     key: oauth_token,
@@ -64,12 +112,15 @@ async function getRequest({
 
 (async () => {
   try {
-    // Make the request
-    console.log('TOKEN', oAuthAccessToken);
-    const response = await getRequest(oAuthAccessToken);
-    console.dir(response, {
-      depth: null
-    });
+    let lyrics = await getRawLyrics();
+    lyrics = filterLyrics(lyrics);
+    const tweet = getTweetFrom(lyrics);
+    console.log('Tweet', tweet);
+
+    // const response = await getRequest(oAuthAccessToken);
+    // console.dir(response, {
+    //   depth: null
+    // });
   } catch (e) {
     console.log(e);
     process.exit(-1);
